358f82c83b689436caed21ff3cb76f2fc1018b91
Bonnie Why <bonnieinpixels@gmail.com>
2018-11-03 22:29:53 -0500

    Everything seems to be working. Still have a few minor cosmetic changes to make. Tomorrow.

--- main.py
+++ main.py
@@ -21,14 +21,14 @@ class Blog(db.Model):
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    blog = Blog.query.all()
+    post_id = request.args.get('id')
 
-    return render_template('index.html', title = "Build a Blog", blog = blog)
-
-@app.route('/blog', methods=['POST', 'GET'])
-def indv_post():
-
-    return render_template('blog.html')
+    if post_id:
+        blog = Blog.query.filter_by(id = post_id).all()
+        return render_template('index.html', blog = blog, post_id = post_id)
+    else:
+        blog = Blog.query.all()
+        return render_template('index.html', title = "Build a Blog", blog = blog)
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def create_new_post():
@@ -40,6 +40,7 @@ def create_new_post():
     if request.method == 'POST':
         blog_title = request.form['blog_title']
         blog_content = request.form['blog_content']
+        new_post = Blog(blog_title, blog_content)
 
         if blog_title == "":
             title_error = "Please enter a title!"
@@ -47,11 +48,11 @@ def create_new_post():
         if blog_content == "":
             content_error = "Please enter a post!"
 
-        else:
-            new_post = Blog(blog_title, blog_content)
+        if title_error == "" and content_error == "":
             db.session.add(new_post)
             db.session.commit()
-            return redirect('/')
+
+            return redirect('/?id={}'.format(new_post.id))
 
     return render_template('newpost.html', title = "Add a new post", blog_title = blog_title, blog_content = blog_content, title_error = title_error, content_error = content_error)
 
